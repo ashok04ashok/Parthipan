1705ab81c017951e1e2010de59525ab9302deee5
ashok04ashok <ashok04ashok@gmail.com>
2025-08-26 21:56:17 +0530

    first commit

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -3,7 +3,7 @@ export const config = {
     specs: ['./src/tests/**/*.ts'],
     maxInstances: 1,
     capabilities: [{
-        browserName: 'chrome',  // works with DevTools
+        browserName: 'chrome',
         acceptInsecureCerts: true
     }],
     logLevel: 'info',
@@ -12,28 +12,10 @@ export const config = {
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
-
-    services: ['devtools'],  // <-- Use devtools instead of selenium or chromedriver
-
+    services: ['devtools'],
     framework: 'mocha',
-    // reporters: ['spec'],
-
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
-    },
-
-    reporters: [
-    'spec',
-    ['allure', {
-        outputDir: 'allure-results', // folder where results are saved
-        disableWebdriverStepsReporting: true,
-        disableWebdriverScreenshotsReporting: false,
-    }]
-],
- afterTest: async function(test, context, { error, result, duration, passed, retries }) {
-        if (!passed) {
-            // await browser.takeScreenshot(); // automatically added to Allure if disableWebdriverScreenshotsReporting=false
-        }
     }
 };
